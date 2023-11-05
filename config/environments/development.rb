a6e3353b7aabfdf3e1878d415f1020f39fc93ecd
Jane Doe <starpall7944@gmail.com>
2023-11-05 06:31:07 +0000

    Updated Gemfile for consistency

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
+
 Rails.application.configure do
+  config.hosts << "d7c3984f38ed41fd8114f170cdf7d955.vfs.cloud9.us-east-1.amazonaws.com"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
